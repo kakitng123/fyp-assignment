bf7655d251810184b69c107f3ba054fe887fe104
ET <entong3378@gmail.com>
2022-11-25 16:00:01 +0800

    latest combine version

--- app/src/main/java/com/example/fyp_booking_application/MenuActivity.kt
+++ app/src/main/java/com/example/fyp_booking_application/MenuActivity.kt
@@ -9,33 +9,31 @@ import com.example.fyp_booking_application.frontend.homeFragment
 import com.example.fyp_booking_application.frontend.profileFragment
 
 class MenuActivity : AppCompatActivity() {
-    /*
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu)
 
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-        replaceFragment(homeFragment())
-
-        binding.mainNavView.setOnItemSelectedListener {
-            when(it.itemId){
-                R.id.navBar_home -> replaceFragment(homeFragment())
-                R.id.navBar_booking -> replaceFragment(bookingFragment())
-                R.id.navBar_profile -> replaceFragment(profileFragment())
-            }
-            true
-        }
+//        binding = ActivityMainBinding.inflate(layoutInflater)
+//        setContentView(binding.root)
+//        replaceFragment(homeFragment())
+//
+//        binding.mainNavView.setOnItemSelectedListener {
+//            when(it.itemId){
+//                R.id.navBar_home -> replaceFragment(homeFragment())
+//                R.id.navBar_booking -> replaceFragment(bookingFragment())
+//                R.id.navBar_profile -> replaceFragment(profileFragment())
+//            }
+//            true
+//        }
+//    }
+//
+//    private fun replaceFragment(fragment: Fragment){
+//        val fragmentManager = supportFragmentManager
+//        val fragmentTransaction = fragmentManager.beginTransaction()
+//        fragmentTransaction.replace(R.id.mainLayout, fragment)
+//        fragmentTransaction.commit()
+//    }
     }
-
-    private fun replaceFragment(fragment: Fragment){
-        val fragmentManager = supportFragmentManager
-        val fragmentTransaction = fragmentManager.beginTransaction()
-        fragmentTransaction.replace(R.id.mainLayout, fragment)
-        fragmentTransaction.commit()
-    }
-
-     */
 }
